9cb8d3a126ad0ffb2939b00737e6c5a7d9fa916d
Tommaso Soru <mommi84@gmail.com>
2015-08-20 19:14:13 +0200

    dataset builder, serializer

---
+++ src/org/aksw/mandolin/semantifier/ACM2Builder.java
@@ -0,0 +1,165 @@
+package org.aksw.mandolin.semantifier;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+import org.aksw.mandolin.util.DataIO;
+
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.Syntax;
+
+/**
+ * @author Tommaso Soru <tsoru@informatik.uni-leipzig.de>
+ *
+ */
+public class ACM2Builder {
+
+	public static final String ORIGIN_ENDPOINT = "http://dblp.l3s.de/d2r/sparql";
+
+	public static final String ELEMENT_REL = "http://purl.org/dc/elements/1.1/creator";
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		new ACM2Builder().run();
+	}
+
+	public void run() throws IOException, ClassNotFoundException {
+		
+		ArrayList<Elements> data = new ArrayList<>();
+
+		for (String rkbURI : getRKBURIs()) {
+
+			Elements e = getElements(rkbURI, ELEMENT_REL, ORIGIN_ENDPOINT);
+
+			System.out.println(e.getURI());
+			
+			if(e.getURI() != null)
+				data.add(e);
+
+		}
+
+		DataIO.serialize(data, "data.map");
+		
+		ArrayList<Elements> data2 = DataIO.readList("data.map");
+		for(Elements e : data2) {
+			System.out.println(e.getURI() + "\t" + e.getElements());
+		}
+		
+//		write(elem);
+
+	}
+
+	/**
+	 * Write element (e.g., author) list to text file.
+	 * 
+	 * @param elem
+	 * @throws FileNotFoundException
+	 */
+	public void write(TreeSet<String> elem) throws FileNotFoundException {
+		PrintWriter pw = new PrintWriter(new File("authors.txt"));
+		for(String e : elem) {
+			pw.write(e+"\n");
+		}
+		pw.close();
+	}
+
+	/**
+	 * Get publication URIs from the perfect mapping.
+	 * 
+	 * @return
+	 * @throws FileNotFoundException
+	 */
+	private ArrayList<String> getRKBURIs() throws FileNotFoundException {
+
+		ArrayList<String> list = new ArrayList<>();
+
+		Scanner in = new Scanner(new File("mappings/dblp-acm.csv"));
+		in.nextLine(); // skip header
+		while (in.hasNextLine()) {
+			String[] line = in.nextLine().split(",");
+			String rkb = line[0].replaceAll("\"", "");
+			list.add("http://dblp.rkbexplorer.com/id/" + rkb);
+		}
+		in.close();
+
+		return list;
+	}
+
+	/**
+	 * Get the publication associated with a list of elements (e.g., authors).
+	 * 
+	 * @param pubURI
+	 * @param elementRel
+	 * @param endpoint
+	 * @return
+	 */
+	private Elements getElements(String pubURI, String elementRel,
+			String endpoint) {
+
+		String query = "SELECT ?cr ?pub WHERE { ?pub <http://www.w3.org/2002/07/owl#sameAs> <"
+				+ pubURI + "> ; <" + elementRel + "> ?cr }";
+//		System.out.println(query);
+		
+		ResultSet rs = sparql(query, endpoint);
+
+		ArrayList<String> list = new ArrayList<>();
+		String uri = null;
+
+		while (rs.hasNext()) {
+			QuerySolution qs = rs.next();
+			uri = qs.getResource("?pub").getURI();
+			list.add(qs.getResource("?cr").getURI());
+		}
+		Elements elem = new Elements(uri);
+		elem.setElements(list);
+
+		return elem;
+
+	}
+
+	private ResultSet sparql(String query, String endpoint) {
+
+		Query sparqlQuery = QueryFactory.create(query, Syntax.syntaxARQ);
+		QueryExecution qexec = QueryExecutionFactory.sparqlService(endpoint,
+				sparqlQuery);
+		return qexec.execSelect();
+
+	}
+
+}
+
+class Elements implements Serializable {
+
+	private static final long serialVersionUID = -4523439946804741035L;
+	
+	private String uri;
+	private List<String> elements;
+
+	public void setElements(List<String> elements) {
+		this.elements = elements;
+	}
+
+	Elements(String uri) {
+		this.uri = uri;
+		elements = new ArrayList<String>();
+	}
+
+	public String getURI() {
+		return uri;
+	}
+
+	public List<String> getElements() {
+		return elements;
+	}
+}
