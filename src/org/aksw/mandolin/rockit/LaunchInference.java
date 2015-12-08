bd93678678a354341410bd29e083b1769185cfdc
Tommaso Soru <mommi84@gmail.com>
2015-12-08 17:35:06 +0100

    Gibbs Sampling is working

--- src/org/aksw/mandolin/rockit/LaunchInference.java
+++ src/org/aksw/mandolin/rockit/LaunchInference.java
@@ -5,21 +5,16 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import com.googlecode.rockit.app.Parameters;
 import com.googlecode.rockit.app.sampler.gibbs.GIBBSLiteral;
 import com.googlecode.rockit.app.sampler.gibbs.GIBBSSampler;
 import com.googlecode.rockit.app.solver.StandardSolver;
-import com.googlecode.rockit.app.solver.exact.ExactSolver;
-import com.googlecode.rockit.app.solver.exact.GroundingSet;
 import com.googlecode.rockit.app.solver.pojo.Clause;
 import com.googlecode.rockit.app.solver.pojo.Literal;
 import com.googlecode.rockit.exception.ParseException;
 import com.googlecode.rockit.exception.ReadOrWriteToFileException;
 import com.googlecode.rockit.exception.SolveException;
-import com.googlecode.rockit.javaAPI.HerbrandUniverse;
 import com.googlecode.rockit.javaAPI.Model;
 import com.googlecode.rockit.parser.SyntaxReader;
 
@@ -29,9 +24,6 @@ import com.googlecode.rockit.parser.SyntaxReader;
  *
  */
 public class LaunchInference {
-
-	// Common
-	private HerbrandUniverse u = HerbrandUniverse.getInstance();
 
 	/**
 	 * MLN file.
@@ -45,14 +37,6 @@ public class LaunchInference {
 
 	private static SyntaxReader reader;
 	private Model model;
-	/**
-	 * This parameter controls by how much the probabilities between exact
-	 * inference and sampling may diverge before we consider them different.
-	 */
-	private static final double delta = 0.005;
-
-	// Exact only
-	private ExactSolver exactSolver;
 
 	// Sampling only
 	/**
@@ -83,17 +67,7 @@ public class LaunchInference {
 	public void run() throws ParseException, IOException, SolveException,
 			SQLException {
 
-		exactSolver = new ExactSolver(model);
-		Map<GroundingSet, Double> exactInferenceResult = exactSolver.solve();
-		HashMap<String, Double> exact_aggregatedResult = exactSolver
-				.aggregate(exactInferenceResult);
 		System.out.println("Input: " + this.input);
-		// System.out.println(exactSolver.getDistribution(exactInferenceResult,
-		// true));
-
-		// Sampling
-		// Model model = reader.getModel(this.input.toString(),
-		// this.groundings.toString());
 		StandardSolver solver = new StandardSolver(model);
 		gibbsSampler = new GIBBSSampler();
 		// ground MLN and retrieve Clauses
@@ -107,21 +81,8 @@ public class LaunchInference {
 		HashMap<String, Double> gibbs_result = new HashMap<String, Double>();
 		for (GIBBSLiteral l : gibbsOutput) {
 			gibbs_result.put(l.getName(), l.return_my_probability(iterations));
-		}
-
-		for (Entry<String, Double> e : exact_aggregatedResult.entrySet()) {
-			Double exactValue = e.getValue();
-			Double gibbsValue = gibbs_result.get(e.getKey());
-
-			System.out.println("gibbsValue = " + gibbsValue);
-
-			// This assumes that the Gibbs sampling is correct, but we do not
-			// know whether the exact inference is. Assumption does not affect
-			// the result.
-			System.out.println(u.transformKeysToConstants(e.getKey())
-					+ ": [Gibbs " + gibbsValue + " ] vs. [" + exactValue
-					+ " Exact] | delta = " + delta);
-
+			System.out.println(l.getName() + ": " + l.return_my_probability(iterations));
+			System.out.println("swap? " + l.is_it_possible_to_swap_me());
 		}
 
 	}
